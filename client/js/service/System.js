f751771c41f366c301092df43f3c7b7fcac9738d
mlmar <marcloussm@gmail.com>
2023-08-22 02:39:08 -0700

    Vercel setup for server functions

--- client/js/service/System.js
+++ client/js/service/System.js
@@ -1,6 +1,6 @@
 const DEV = false;
 
 const LOCAL_SERVER = 'http://192.168.0.89:3300';
-const DEPLOYED_SERVER = 'https://multiply-auth.vercel.app';
+const DEPLOYED_SERVER = 'https://mlmar-multiply.vercel.app';
 
 export const SERVER_URL = DEV ? LOCAL_SERVER : DEPLOYED_SERVER;
